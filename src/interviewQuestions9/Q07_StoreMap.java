0653133031ca9a961b77e71ffde05cdb97879f13
Meryem Akay <haceryol491@gmail.com>
2022-05-04 11:20:08 +0300

    04.05.2022

--- src/interviewQuestions9/Q07_StoreMap.java
+++ src/interviewQuestions9/Q07_StoreMap.java
@@ -40,9 +40,9 @@ public class Q07_StoreMap {
 
         System.out.println("gormek istediginiz kisinin kimlik numarasini giriniz : ");
         System.out.println(kisiListesi.get(scan.nextInt()));
-
-
+        System.out.println(kisiListesi.get(scan.nextInt()));
 
     }
+
 
 }
